a8fba0f6a0d878c71f99d3ea970b2119771a18ba
oman ramalho araujo da silva <omanapple42@hotmail.com>
2023-07-28 12:52:07 -0300

    Create Register Screen

---
+++ app/register.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { View, Text } from 'react-native'
+
+const register:React.FC = () => {
+  return (
+    <View>
+      <Text>
+        register
+      </Text>
+    </View>
+  )
+}
+
+export default register
