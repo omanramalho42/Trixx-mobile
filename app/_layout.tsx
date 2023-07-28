ade228205e8f3f05f131d9479257c7ac778e2a39
oman ramalho araujo da silva <omanapple42@hotmail.com>
2023-07-28 12:20:14 -0300

    First commit

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Stack } from 'expo-router'
+
+const Layout = () => {
+  return <Stack />
+}
+
+export default Layout
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello World! 🦇🔥</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
