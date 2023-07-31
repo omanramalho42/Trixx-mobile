e75339f38df236830cced1009966197c20348c18
oman ramalho araujo da silva <omanapple42@hotmail.com>
2023-07-30 22:29:54 -0300

    creating stylesheet register screen

---
+++ styles/Register.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components/native";
+
+export const Container = styled.ScrollView`
+  display: flex;
+  flex-direction: column;
+
+  padding: 22px 16px;
+`;
+
+export const Logo = styled.Image.attrs({
+  source: require('../assets/Logo2.png'),
+  alt: "Logo"
+})`
+  width: 112.745px;
+  height: 112.745px;
+
+  margin-top: 9px;
+  margin-bottom: 3px;
+`;
+
+export const ContentInput = styled.View`
+  width: 100%;
+  
+  display: flex;
+  flex-direction: column;
+
+  margin: 5px 0 10px 0;
+
+  padding-top: 2px;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+  height: 48.578px;
+
+  border-radius: 8px;
+  border: 1px solid #666;
+
+  border-radius: 8px;
+
+  padding: 16px 15px;
+`;
+
+export const Label = styled.Text`
+  margin-bottom: 10px;
+  margin-top: 6px;
+
+  color: #666;
+
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+
+  /* font-family: "Poppins-Bold"; */
+`;
